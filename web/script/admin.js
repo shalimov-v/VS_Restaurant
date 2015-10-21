18f81f3d4fcf9fa6da9da7194c5daad228fc6027
shalimov-v <visjaka@yandex.ru>
2015-10-21 13:20:29 +0300

    Admin panel added to design

---
+++ web/script/admin.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+    var area = $('#leftArea');
+
+    area.find('#entityList #employee').click(function(){
+        $("#leftArea").find("#entityList li").css({'color' : '#333333'});
+        $(this).css({'color' : '#FF0084'});
+        $.ajax({
+            type: 'POST',
+            url: '/pages/admin_employee.html',
+            cache: false,
+            success: function(html){
+                $('#mainArea').html(html);
+            }
+        });
+    });
+
+    area.find('#entityList #desk').click(function(){
+        $("#leftArea").find("#entityList li").css({'color' : '#333333'});
+        $(this).css({'color' : '#FF0084'});
+        $.ajax({
+            type: 'POST',
+            url: '/pages/admin_desk.html',
+            cache: false,
+            success: function(html){
+                $('#mainArea').html(html);
+            }
+        });
+    });
+
+    area.find('#entityList #dishType').click(function(){
+        $("#leftArea").find("#entityList li").css({'color' : '#333333'});
+        $(this).css({'color' : '#FF0084'});
+        $.ajax({
+            type: 'POST',
+            url: '/pages/admin_dishType.html',
+            cache: false,
+            success: function(html){
+                $('#mainArea').html(html);
+            }
+        });
+    });
+
+    area.find('#entityList #dish').click(function(){
+        $("#leftArea").find("#entityList li").css({'color' : '#333333'});
+        $(this).css({'color' : '#FF0084'});
+        $.ajax({
+            type: 'POST',
+            url: '/pages/admin_dish.html',
+            cache: false,
+            success: function(html){
+                $('#mainArea').html(html);
+            }
+        });
+    });
+
+    area.find('#entityList #menus').click(function(){
+        $("#leftArea").find("#entityList li").css({'color' : '#333333'});
+        $(this).css({'color' : '#FF0084'});
+        $.ajax({
+            type: 'POST',
+            url: '/pages/admin_menu.html',
+            cache: false,
+            success: function(html){
+                $('#mainArea').html(html);
+            }
+        });
+    });
+
+});
--- web/script/index.js
+++ web/script/index.js
@@ -1,93 +1,77 @@
 $(document).ready(function(){
 
-    var area = $('#leftArea');
+    var $menu = $('#menu');
 
-    area.find('#entityList #employee').click(function(){
-        $("#leftArea").find("#entityList li")
-            .css({'border' : 'solid #dddddd 1px'})
-            .css({'color' : '#0063DC'});
+    $.ajax({
+        type: "POST",
+        url: "/pages/homepage_content.html",
+        cache: false,
+        success: function(html){
+            $("#content").html(html);
+        }
+    });
 
-        $(this)
-            .css({'color' : '#FF0084'})
-            .css({'border' : 'none'});
+    $menu.find('#menuItems #homepage').click(function(){
+        $('#menu').find('#menuItems li').css({'background': 'none'});
+        $(this).css({'background': 'url(../images/img03.gif) no-repeat center bottom'});
         $.ajax({
-            type: 'POST',
-            url: '/pages/employee.html',
+            type: "POST",
+            url: "/pages/homepage_content.html",
             cache: false,
             success: function(html){
-                $('#mainArea').html(html);
+                $("#content").html(html);
             }
         });
     });
 
-    area.find('#entityList #desk').click(function(){
-        $("#leftArea").find("#entityList li")
-            .css({'border' : 'solid #dddddd 1px'})
-            .css({'color' : '#0063DC'});
-
-        $(this)
-            .css({'color' : '#FF0084'})
-            .css({'border' : 'none'});
+    $menu.find('#menuItems #menus').click(function(){
+        $('#menu').find('#menuItems li').css({'background': 'none'});
+        $(this).css({'background': 'url(../images/img03.gif) no-repeat center bottom'});
         $.ajax({
-            type: 'POST',
-            url: '/pages/desk.html',
+            type: "POST",
+            url: "/pages/menu_content.html",
             cache: false,
             success: function(html){
-                $('#mainArea').html(html);
+                $("#content").html(html);
             }
         });
     });
 
-    area.find('#entityList #dishType').click(function(){
-        $("#leftArea").find("#entityList li")
-            .css({'border' : 'solid #dddddd 1px'})
-            .css({'color' : '#0063DC'});
-
-        $(this)
-            .css({'color' : '#FF0084'})
-            .css({'border' : 'none'});
+    $menu.find('#menuItems #reserve').click(function(){
+        $('#menu').find('#menuItems li').css({'background': 'none'});
+        $(this).css({'background': 'url(../images/img03.gif) no-repeat center bottom'});
         $.ajax({
-            type: 'POST',
-            url: '/pages/dishType.html',
+            type: "POST",
+            url: "/pages/reserve_content.html",
             cache: false,
             success: function(html){
-                $('#mainArea').html(html);
+                $("#content").html(html);
             }
         });
     });
 
-    area.find('#entityList #dish').click(function(){
-        $("#leftArea").find("#entityList li")
-            .css({'border' : 'solid #dddddd 1px'})
-            .css({'color' : '#0063DC'});
-
-        $(this)
-            .css({'color' : '#FF0084'})
-            .css({'border' : 'none'});
+    $menu.find('#menuItems #contacts').click(function(){
+        $('#menu').find('#menuItems li').css({'background': 'none'});
+        $(this).css({'background': 'url(../images/img03.gif) no-repeat center bottom'});
         $.ajax({
-            type: 'POST',
-            url: '/pages/dish.html',
+            type: "POST",
+            url: "/pages/contact_content.html",
             cache: false,
             success: function(html){
-                $('#mainArea').html(html);
+                $("#content").html(html);
             }
         });
     });
 
-    area.find('#entityList #menu').click(function(){
-        $("#leftArea").find("#entityList li")
-            .css({'border' : 'solid #dddddd 1px'})
-            .css({'color' : '#0063DC'});
-
-        $(this)
-            .css({'color' : '#FF0084'})
-            .css({'border' : 'none'});
+    $menu.find('#menuItems #admin').click(function(){
+        $('#menu').find('#menuItems li').css({'background': 'none'});
+        $(this).css({'background': 'url(../images/img03.gif) no-repeat center bottom'});
         $.ajax({
-            type: 'POST',
-            url: '/pages/menu.html',
+            type: "POST",
+            url: "/pages/admin_content.html",
             cache: false,
             success: function(html){
-                $('#mainArea').html(html);
+                $("#content").html(html);
             }
         });
     });

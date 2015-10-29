ac502cf102c15fe3d6dcab3707b519b082057b3f
shalimov-v <visjaka@yandex.ru>
2015-10-29 12:13:27 +0200

    Admin Panel Client Management Improved

--- web/script/admin_client.js
+++ web/script/admin_client.js
@@ -32,6 +32,130 @@ function loadAllDiscounts(id) {
     });
 }
 
+function findClientById(id) {
+    $.ajax({
+        type: 'GET',
+        url: rootURL + '/' + id,
+        dataType: "json",
+        success: function(data){
+            $('#btnDelete').show();
+            currentClient = data;
+            renderClientDetails(currentClient);
+        }
+    });
+}
+
+function createClient() {
+    var clientId = $mainArea.find('#clientId').val();
+    var clientDiscountId = $mainArea.find('#discountList').val();
+    var clientFirstName = $mainArea.find('#clientFirstName').val();
+    var clientLastName = $mainArea.find('#clientLastName').val();
+    $.ajax({
+        type: 'GET',
+        url: 'api/discounts/' + clientDiscountId,
+        dataType: "json",
+        success: function(data){
+            var discount = {
+                "clientId": clientId == "" ? null : clientId,
+                "clientFirstName": clientFirstName,
+                "clientLastName": clientLastName,
+                "clientDiscount": data
+            };
+            $.ajax({
+                type: 'POST',
+                contentType: 'application/json',
+                url: rootURL,
+                dataType: "json",
+                data: JSON.stringify(discount),
+                success: function(data, textStatus, jqXHR){
+                    clearClientDetails();
+                    loadAllClients();
+                    alert('Record created successfully');
+                },
+                error: function(jqXHR, textStatus, errorThrown){
+                    clearClientDetails();
+                    loadAllClients();
+                    alert('Error while creating record: ' + textStatus);
+                }
+            });
+        }
+    });
+}
+
+function updateClient() {
+    var clientId = $mainArea.find('#clientId').val();
+    var clientDiscountId = $mainArea.find('#discountList').val();
+    var clientFirstName = $mainArea.find('#clientFirstName').val();
+    var clientLastName = $mainArea.find('#clientLastName').val();
+    $.ajax({
+        type: 'GET',
+        url: 'api/discounts/' + clientDiscountId,
+        dataType: "json",
+        success: function(data){
+            var discount = {
+                "clientId": clientId == "" ? null : clientId,
+                "clientFirstName": clientFirstName,
+                "clientLastName": clientLastName,
+                "clientDiscount": data
+            };
+            $.ajax({
+                type: 'PUT',
+                contentType: 'application/json',
+                url: rootURL,
+                dataType: "json",
+                data: JSON.stringify(discount),
+                success: function(data, textStatus, jqXHR){
+                    clearClientDetails();
+                    loadAllClients();
+                    alert('Record updated successfully');
+                },
+                error: function(jqXHR, textStatus, errorThrown){
+                    clearClientDetails();
+                    loadAllClients();
+                    alert('Error while updating record: ' + textStatus);
+                }
+            });
+        }
+    });
+}
+
+function deleteClient() {
+    var clientId = $mainArea.find('#clientId').val();
+    var clientDiscountId = $mainArea.find('#discountList').val();
+    var clientFirstName = $mainArea.find('#clientFirstName').val();
+    var clientLastName = $mainArea.find('#clientLastName').val();
+    $.ajax({
+        type: 'GET',
+        url: 'api/discounts/' + clientDiscountId,
+        dataType: "json",
+        success: function(data){
+            var discount = {
+                "clientId": clientId == "" ? null : clientId,
+                "clientFirstName": clientFirstName,
+                "clientLastName": clientLastName,
+                "clientDiscount": data
+            };
+            $.ajax({
+                type: 'DELETE',
+                contentType: 'application/json',
+                url: rootURL,
+                dataType: "json",
+                data: JSON.stringify(discount),
+                success: function(data, textStatus, jqXHR){
+                    clearClientDetails();
+                    loadAllClients();
+                    alert('Record deleted successfully');
+                },
+                error: function(jqXHR, textStatus, errorThrown){
+                    clearClientDetails();
+                    loadAllClients();
+                    alert('Error while deleting record: ' + textStatus);
+                }
+            });
+        }
+    });
+}
+
 function renderClientList(data) {
     var list = data == null ? [] : (data instanceof Array ? data : [data]);
     $mainArea.find('#clientList option').remove();
@@ -41,6 +165,29 @@ function renderClientList(data) {
     });
 }
 
+function renderClientDetails(client) {
+    loadAllDiscounts(client.clientDiscount.discountId);
+    $mainArea.find('#clientId').val(client.clientId);
+    $mainArea.find('#clientFirstName').val(client.clientFirstName);
+    $mainArea.find('#clientLastName').val(client.clientLastName);
+}
+
+function clearClientDetails() {
+    loadAllDiscounts(0);
+    $mainArea.find('#clientId').val('');
+    $mainArea.find('#clientFirstName').val('');
+    $mainArea.find('#clientLastName').val('');
+    $('#btnDelete').hide();
+}
+
+function selectClient() {
+    var id = $('#mainArea').find('#clientList').val();
+    if (id != 0) {
+        findClientById(id);
+        $('#btnDelete').show();
+    }
+}
+
 $(document).ready(function(){
 
     loadAllClients();
@@ -48,5 +195,25 @@ $(document).ready(function(){
     loadAllDiscounts(0);
 
     $mainArea.find('#btnDelete').hide();
+
+    $mainArea.find('#btnSave').click(function(){
+        if ($mainArea.find('#clientFirstName').val() == '' || $mainArea.find('#discountList').val() == 0 || $mainArea.find('#clientFirstName').val() == '')  {
+            alert('Please fill all fields');
+        } else {
+            if ($mainArea.find('#clientId').val() == '') {
+                createClient();
+            } else {
+                updateClient();
+            }
+        }
+        return false;
+    });
+
+    $mainArea.find('#btnDelete').click(function(){
+        deleteClient();
+        clearClientDetails();
+        loadAllClients();
+        return false;
+    });
 
 });
